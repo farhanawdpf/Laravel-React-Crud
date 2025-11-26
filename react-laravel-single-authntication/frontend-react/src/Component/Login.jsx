0f59f352d59d0ba89bbc7c48f05a36740d53df49
farhanawdpf <farhanawdpf@gmail.com>
2025-11-26 10:55:07 +0600

    update

--- react-laravel-single-authntication/frontend-react/src/Component/Login.jsx
+++ react-laravel-single-authntication/frontend-react/src/Component/Login.jsx
@@ -9,7 +9,6 @@ const Login = () => {
   const submitForm = async (e) => {
     e.preventDefault();
     const res = await axios.post("http://127.0.0.1:8000/api/login", form);
-
     localStorage.setItem("token", res.data.token);
     alert("Logged in!");
   };
--- react-laravel-single-authntication/frontend-react/src/Component/Register.jsx
+++ react-laravel-single-authntication/frontend-react/src/Component/Register.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
 import { useState } from "react";
 const Register = () => {
-    const [form, setForm] = useState({name: "", email: "", password: ""});
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
 
   const handleChange = (e) => {
-      setForm({...form, [e.target.name]: e.target.value});
-  }
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
- const submit = async (e) => {
+  const submit = async (e) => {
     e.preventDefault();
     const res = await axios.post("http://127.0.0.1:8000/api/register", form);
     localStorage.setItem("token", res.data.token);
@@ -16,19 +16,46 @@ const Register = () => {
   };
 
   return (
-    <div className='container'> 
-    <form onSubmit={submit}><br />
-    Name:
-      <input name="name" onChange={handleChange} /><br />
-      Email:
-      <input name="email" onChange={handleChange} /><br />
-      Password:
-      <input name="password" type="password" onChange={handleChange} />
-      <button>Register</button>
-    </form>
-
+    <div className="container">
+      <div className="row justify-content-center pt-5">
+        <div className="col-sm-6">
+          <div className="card p-4">
+            <h1 className="text-center mb-3">Register Form </h1>
+            <form onSubmit={submit}>
+              <br />
+              <div className="form-group">
+                <label>Name:</label>
+                <input
+                  name="name"
+                  className="form-control"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label>Email Address:</label>
+                <input
+                  name="email"
+                  className="form-control"
+                  onChange={handleChange}
+                />
+                <br />
+              </div>
+              <div className="form-group">
+                <label>Password:</label>
+                <input
+                  name="password"
+                  className="form-control"
+                  type="password"
+                  onChange={handleChange}
+                />
+              </div>
+              <button className="btn btn-primary mt-4">Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
